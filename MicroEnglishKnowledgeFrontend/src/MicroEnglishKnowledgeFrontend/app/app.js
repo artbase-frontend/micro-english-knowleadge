e76401edc4d8989fd1c68971bc03f60c557c5b5a
artbase <bartosz.zaremba@live.com>
2016-06-03 23:21:47 +0200

    Add simple project with frontend

---
+++ MicroEnglishKnowledgeFrontend/src/MicroEnglishKnowledgeFrontend/app/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('microEnglishKnowledge', [
+        // Angular modules 
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+})();
---
+++ MicroEnglishKnowledgeFrontend/src/MicroEnglishKnowledgeFrontend/app/main/main.controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('microEnglishKnowledge')
+        .controller('mainController', main);
+
+    main.$inject = ['$scope']; 
+
+    function main($scope) {
+        $scope.title = 'Welcome to micro english';
+
+        activate();
+
+        function activate() { }
+    }
+})();
